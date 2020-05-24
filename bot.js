a9fbea17c15470fbac51635534fe7e0b8ebb6b13
codboy7 <64877522+codboy7@users.noreply.github.com>
2020-05-24 14:59:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
-const token = 'NzE0MTk2MTM2NTU4MTk4ODE2.XsrJfg.Gy9NHhUIEmkG_HDI9PZp60zaNog';
+const token = '';
 
 client.on('ready', () => {
 	console.log('I am ready!');
   });
   client.on('message', message => {
-	if (message.author.id === '457241709982646283') {
+	if (message.author.id === '') {
 	  message.reply('hey stupid!');
 	}
   });
